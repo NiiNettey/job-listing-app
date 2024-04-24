13b3c7dd9e894e41ae92200177423e541c693142
James Nerquaye-Tetteh <james.nerquaye-tetteh@amalitech.com>
2024-04-24 10:29:13 +0000

    feat add jobs component

--- src/app/components/jobs/jobs.component.ts
+++ src/app/components/jobs/jobs.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { JobDataService } from '../../services/job-data.service';
+import { Job } from '../../interface/interface';
+import { JobSearchService } from '../../services/job-search.service';
+import { NgFor } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-jobs',
@@ -6,6 +12,7 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
+  imports: [CardComponent, NgFor]
 })
 export class JobsComponent {
 
