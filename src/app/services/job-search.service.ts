744595305022210cee19bed8ae2bb11762b6a8e5
James Nerquaye-Tetteh <james.nerquaye-tetteh@amalitech.com>
2024-04-24 10:37:59 +0000

    created job and search servies

---
+++ src/app/services/job-search.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobSearchService {
+  private _myTags: string[] = [];
+  public myTags$ = new BehaviorSubject<string[]>(this._myTags);
+
+  get myTags(): string[] {
+    return this._myTags;
+  }
+
+  set myTags(value: string[]) {
+    this._myTags = value;
+    this.myTags$.next(this._myTags);
+  }
+
+  constructor() { }
+}
