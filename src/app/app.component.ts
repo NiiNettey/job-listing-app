65e849fcf18d829e3994ba80c33667a3331eaa0d
James Nerquaye-Tetteh <james.nerquaye-tetteh@amalitech.com>
2024-04-24 10:20:43 +0000

    imported components in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { CardComponent } from "./components/card/card.component";
+import { JobsComponent } from "./components/jobs/jobs.component";
+import { FilterBarComponent } from "./components/filter-bar/filter-bar.component";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent, JobsComponent, CardComponent, FilterBarComponent]
 })
 export class AppComponent {
   title = 'job-listing-app';
